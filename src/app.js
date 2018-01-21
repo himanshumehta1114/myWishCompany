abce53e560c854850397993313e8a131303c31f7
himanshu <himanshumehta1114@gmail.com>
2018-01-22 01:21:37 +0530

    Removed show password toggle

--- src/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('myApp', ['ui.bootstrap']);
-
-app.controller('MainCtrl', function($scope) {
-    $scope.params = {};
-    $scope.showPassword = false;
-    $scope.toggleShowPassword = function() {
-        $scope.showPassword = !$scope.showPassword;
-    }
-});
